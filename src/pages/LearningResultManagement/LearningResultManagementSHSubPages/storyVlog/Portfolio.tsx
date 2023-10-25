c7a570df545a9a8d64922d3b997b00ec15f70565
Alisher Karim <alisherkarim@Alishers-MacBook-Air.local>
2023-10-25 20:19:59 +0900

    fix issue #14551

--- src/pages/LearningResultManagement/LearningResultManagementSHSubPages/storyVlog/Portfolio.tsx
+++ src/pages/LearningResultManagement/LearningResultManagementSHSubPages/storyVlog/Portfolio.tsx
@@ -63,7 +63,7 @@ const Portfolio = () => {
     const LessonVlog: FC<{lesson: TLRMSpeakingHubStoryVlogLesson}> = ({lesson}) => {
         return (
             <div className="mt-[14px] w-[660px] h-[346px]">
-                <video width="660" height="100%" controls >
+                <video width="660" style={{height: '100%'}} controls>
                     <source src={lesson.video} type="video/mp4"/>
                 </video>
             </div>
